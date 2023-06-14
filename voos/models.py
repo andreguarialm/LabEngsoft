47dc26881439196aaaabe7ca53509119f81eb425
osusk <seikiguilherme@usp.br>
2023-06-14 18:45:41 -0300

    Admin add VooPioto

--- voos/models.py
+++ voos/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class VooPiloto(models.Model):
     data = models.DateField()
-    hora_chegada = models.DateTimeField()
-    hora_saida = models.DateTimeField()
+    hora_chegada = models.TimeField()
+    hora_saida = models.TimeField()
 
 class VooAluno(models.Model):
     data = models.DateField()
-    hora_chegada = models.DateTimeField()
-    hora_saida = models.DateTimeField()
+    hora_chegada = models.TimeField()
+    hora_saida = models.TimeField()
     parecer = models.CharField(max_length=200)
