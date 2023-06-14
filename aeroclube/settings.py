137b18963926754545d01d92b9ee10cda03b973e
andreguarialm <andreguarialm@gmail.com>
2023-06-14 20:36:09 -0300

    pare de ser teimoso git

--- aeroclube/settings.py
+++ aeroclube/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'voos.apps.VoosConfig'
+    'voos.apps.VoosConfig',
     'usuarios.apps.UsuariosConfig'
 ]
 
