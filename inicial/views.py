9708f5e7cb5b88f31c91ec499a6cdc018738992f
IsaacYusuke <isaacyusuke@usp.br>
2023-06-16 17:04:06 -0300

    Isaac

--- inicial/views.py
+++ inicial/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from usuarios.models import Socio
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 # Create your views here.
 
 #@login_required
 def home(request):    
-    return render(request, 'inicial/home.html')
+    if Socio.objects.filter(user = request.user).exists():
+        socio = request.user.socio
+        voos = socio.voos.order_by("-hora_saida")
+        ctx =  {
+            "voos": [(len(voos) - i, voo) for i, voo in enumerate(voos)], 
+            "socio": socio,
+            #"duracao": socio.voos.hora_saida - socio.voos.hora_chegada 
+            "duracao": socio.voos
+        }    
+    else:
+        ctx={}
+    return render(request, 'inicial/home.html', ctx)
--- voos/models.py
+++ voos/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from datetime import datetime
+from django.db.models.functions import Extract
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
 # class VooPiloto(models.Model):
@@ -17,9 +21,13 @@ class Voo(models.Model):
     socio = models.ForeignKey("usuarios.Socio", on_delete=models.CASCADE, related_name="voos")
     hora_chegada = models.DateTimeField()
     hora_saida = models.DateTimeField()
+    #duracao = hora_saida.strftime('%Y') - hora_chegada.strftime('%Y')
+    duracao = (Extract(hora_chegada, "hour") - Extract(hora_saida, "hour"))
     
     def __str__(self):
-        return f"({self.socio}) {self.hora_saida} - {self.hora_chegada}"
+       return f"({self.socio}) {self.hora_saida} - {self.hora_chegada}"
+    #def __str__(self):
+    #   return f"{self.duracao.strftime('%Y')}"
     
 class AcompanhamentoVoo(models.Model):
     voo = models.OneToOneField("voos.Voo", on_delete=models.CASCADE, related_name="acompanhamento")
